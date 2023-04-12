7c9fc7e89049fdd31ed4c0d4500b06260b013fa5
ricksterhd123 <ricksterhd123@gmail.com>
2023-04-12 21:06:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# samscan
+scan your sam with sam scan
+
+## Setup
+```bash
+chmod +x setup.sh
+./setup.sh
+# Add path
+export PATH=$PATH:$HOME/bin/samscan
+```
+
+## Usage
+`samscan [FILE]`
+
+Currently, this has only one use for me:
+- Echos to stdout a list of resource types used in a given template
+- Errors if template is not valid (naive check)
+
+## TODO?
+- [ ] Add more scanners, e.g. find and list permissions from IAM policies
---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+if ! command -v python3 &> /dev/null
+then
+    echo "Command python3 could not be found"
+    exit
+fi
+
+if ! command -v jq &> /dev/null
+then
+    echo "Command jq could not be found"
+    exit
+fi
+
+python3 -m venv $SCRIPT_DIR/venv
+source $SCRIPT_DIR/venv/bin/activate
+pip install yq
+chmod +x samscan
+mkdir -p ~/bin/samscan
+cp $SCRIPT_DIR/samscan ~/bin/samscan/
+cp -R $SCRIPT_DIR/venv ~/bin/samscan/
+echo "Setup finished. You need to add to the command to PATH, typing 'export PATH=\$PATH":\$HOME/bin/samscan"'"
